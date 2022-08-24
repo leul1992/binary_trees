4d8fc077c6bdda95d5b797ae2d9577dd90e5ba1e
Leulseged <leulbekele191@gmail.com>
2022-08-24 22:00:21 +0300

    check if root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,11 +4,11 @@
  * @node: pointer to the node to check
  * Return: 1(success) or 0(failure)
  */
- int binary_tree_is_root(const binary_tree_t *node)
- {
-    if (!node || node->parent)
-    {
-        return (0);
-    }
-    return (1);
- }
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node || node->parent)
+	{
+		return (0);
+	}
+	return (1);
+}

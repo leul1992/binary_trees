f76bebc7529042128121db5b0a976231c6bd042e
Leulseged <leulbekele191@gmail.com>
2022-08-25 07:35:50 +0300

    find height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 #include <stdio.h>
+
+
 /**
  * binary_tree_height-measures height of the binary tree
  * @tree: pointer to the root node
@@ -7,18 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (!tree || !(tree->left || tree->right))
 		return (0);
 	int left_height = binary_tree_height(tree->left);
 	int right_height = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
-	{
-        left_height--;
+	if (left_height >= right_height)
 		return (left_height + 1);
-	}
 	else
-    {
 		return (right_height + 1);
-    }
 }

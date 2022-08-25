c24f75630939b2f386da7cb31b840b56d9cbd4e1
Leulseged <leulbekele191@gmail.com>
2022-08-25 07:29:36 +0300

    find height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,17 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (-1);
+		return (0);
 	int left_height = binary_tree_height(tree->left);
 	int right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 	{
+        left_height--;
 		return (left_height + 1);
 	}
 	else
+    {
 		return (right_height + 1);
+    }
 }

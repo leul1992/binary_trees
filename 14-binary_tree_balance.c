96cc8ca94a07ee837e541c9f9a3b8aa6230c0173
Leulseged <leulbekele191@gmail.com>
2022-08-26 08:41:48 +0300

    measure balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,10 +6,13 @@
  */
 int height(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_height;
+
 	if (tree == NULL || !(tree->left || tree->right))
 		return (0);
-	int left_height = height(tree->left);
-	int right_height = height(tree->right);
+	left_height = height(tree->left);
+	right_height = height(tree->right);
 
 	if (left_height >= right_height)
 		return (left_height + 1);
@@ -23,12 +26,15 @@ int height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left;
+	int right;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	int left = height(tree->left) + (tree->left ? 1 : 0);
-	int right = height(tree->right) + (tree->right ? 1 : 0);
+	left = height(tree->left) + (tree->left ? 1 : 0);
+	right = height(tree->right) + (tree->right ? 1 : 0);
 
 	return (left - right);
 }
